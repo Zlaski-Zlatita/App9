859126fd33f81bf10d9a08e394b7e42d6878241d
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2024-09-17 21:58:07 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -40,7 +40,11 @@ TEMPLATES = [
         {
                 'BACKEND': 'django.template.backends.django.DjangoTemplates',
                 'DIRS': [],
-                'APP_DIRS': True
-                
+                'APP_DIRS': True,
+                'OPTIONS': {
+                        'context_processors': [
+                                'django.template.context.processors.debug',
+                                'django.template.context.processors.request',
+                                'django.contrib.messages.auth.
   
 
