499e11cb26572e39bcdc96a196c76397b93f2c06
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2024-09-17 20:34:54 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,28 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET KEY = 'dti-35gg'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True 
+
+ALLOWED_HOSTS = []
+
+
+# Application definition 
+
+INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+        'django.middleware.settingsSecurityMiddleware',
+        'django.contrib.sessions.middlewareSessionMiddleware',
+        'django.contrib.common.CommonMiddleware',
+  
+
