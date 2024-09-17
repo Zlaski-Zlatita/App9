ba55fb0a93b3b10c4ce46a8f3598ea5d3fc41e04
Zlaski-Zlatita <169074132+Zlaski-Zlatita@users.noreply.github.com>
2024-09-17 19:20:18 +0500

    Update URLs.py

--- URLs.py
+++ URLs.py
@@ -1,2 +1,8 @@
 """taskmanager URL Configuration 
 
+The 'urlpatterns' list routes URLs to views. For Information please see:
+  http://docs.djangoproject.com/en/3.0/topics/urls/
+Examples:
+Function views 
+  1. Add an import: from my_app import views
+  2. Add a URL urlpatterns: 
